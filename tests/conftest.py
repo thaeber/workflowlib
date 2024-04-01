e2f2bcf13621342e4c71583ad82c4c5782dc0b9e
thaeber <40823270+thaeber@users.noreply.github.com>
2024-04-01 08:01:52 +0200

    Restructured folders

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 import pytest
-from metalib2 import Metadata
 
 
 @pytest.fixture(scope='function')
 def data_path():
     path = Path(__file__).parent / 'data'
-    return path
+    return path.absolute()
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -29,9 +29,32 @@ class TestLoader:
             )
         )
         assert len(sources) == 4
-        assert sources == [
-            data_path / 'eurotherm/20240118T084901.txt',
-            data_path / 'eurotherm/20240118T085901.txt',
-            data_path / 'eurotherm/20240118T090901.txt',
-            data_path / 'eurotherm/20240118T091901.txt',
-        ]
+        assert sorted(sources) == sorted(
+            [
+                data_path / 'eurotherm/20240118T084901.txt',
+                data_path / 'eurotherm/20240118T085901.txt',
+                data_path / 'eurotherm/20240118T090901.txt',
+                data_path / 'eurotherm/20240118T091901.txt',
+            ]
+        )
+
+    def test_source_with_recursion(self, data_path: Path):
+        loader = Loader(
+            name='loader',
+            version='v1.0',
+        )
+
+        sources = list(
+            loader.get_sources(
+                source=data_path / 'eurotherm/subfolder/**/*.txt',
+            )
+        )
+        assert len(sources) == 4
+        assert sorted(sources) == sorted(
+            [
+                data_path / 'eurotherm/subfolder/a/20240118T084901.txt',
+                data_path / 'eurotherm/subfolder/b/20240118T085901.txt',
+                data_path / 'eurotherm/subfolder/c/20240118T090901.txt',
+                data_path / 'eurotherm/subfolder/d/20240118T091901.txt',
+            ]
+        )
