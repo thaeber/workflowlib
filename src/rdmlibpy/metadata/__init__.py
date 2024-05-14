ced94c75065d380d9f8a4033c688c791d2876bf0
thaeber <40823270+thaeber@users.noreply.github.com>
2024-05-14 11:13:48 +0200

    fix: Missing import of load_yaml function

--- src/rdmlibpy/metadata/__init__.py
+++ src/rdmlibpy/metadata/__init__.py
@@ -1,7 +1,7 @@
 # set package version
 __version__ = '0.1.1'
 
-from .metadata import Metadata, MetadataDict, MetadataList, MetadataNode
+from .metadata import Metadata, MetadataDict, MetadataList, MetadataNode, load_yaml
 from .queries import query
 from .resolvers import register_custom_resolvers
 
@@ -11,6 +11,7 @@ register_custom_resolvers()
 __all__ = [
     Metadata,
     MetadataNode,
+    load_yaml,
     query,
     register_custom_resolvers,
 ]  # type: ignore
