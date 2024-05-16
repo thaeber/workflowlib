902baa731f6debc33cb33b6efa2d41c6453a5bf6
thaeber <40823270+thaeber@users.noreply.github.com>
2024-05-16 12:02:15 +0200

    feat: Metadata resolver to subtract a given timedelta value from a timestamp

--- src/rdmlibpy/metadata/resolvers.py
+++ src/rdmlibpy/metadata/resolvers.py
@@ -1,3 +1,6 @@
+import re
+
+import numpy as np
 from omegaconf import DictConfig, ListConfig, OmegaConf
 from omegaconf.base import Box
 from omegaconf.errors import OmegaConfBaseException
@@ -18,5 +21,23 @@ def get_metadata_property(name, *, _parent_):
     return _get_inherited_property(name, _parent_)
 
 
+def _parse_time_delta_string(value: str):
+    match = re.match(r'\s*(\d+)\s*(ms|us|ns|ps|fs|as|D|h|m|s)', value)
+    if match:
+        number, unit = match.groups()
+        return np.timedelta64(number, unit)
+    else:
+        raise ValueError(f'>{value}< is not a valid time delta.')
+
+
+def subtract_timedelta(timestamp: str, value: str):
+    dt = np.datetime64(timestamp)
+    delta = _parse_time_delta_string(value)
+    return str(dt - delta)
+
+
 def register_custom_resolvers():
     OmegaConf.register_new_resolver('meta.get', get_metadata_property, replace=True)
+    OmegaConf.register_new_resolver(
+        'meta.subtract-timedelta', subtract_timedelta, replace=True
+    )
--- tests/metadata/test_resolvers.py
+++ tests/metadata/test_resolvers.py
@@ -1,4 +1,8 @@
+import numpy as np
+import pytest
+
 from rdmlibpy.metadata import Metadata
+from rdmlibpy.metadata.resolvers import _parse_time_delta_string
 
 
 class TestMetaGetResolver:
@@ -21,3 +25,70 @@ class TestMetaGetResolver:
         meta = Metadata.create(yaml)
         assert meta.data[0].steps[0].params.start == 'a1234'
         assert meta.data[0].steps[1].params.date == '2024-01-16'
+
+
+class TestMetaSubtractTimeDelta:
+    def test_string_parsing(self):
+        assert _parse_time_delta_string('1m') == np.timedelta64(1, 'm')
+        assert _parse_time_delta_string('1 m') == np.timedelta64(1, 'm')
+        assert _parse_time_delta_string('  1 m  ') == np.timedelta64(1, 'm')
+
+        assert _parse_time_delta_string('1D') == np.timedelta64(1, 'D')
+        assert _parse_time_delta_string('1m') == np.timedelta64(1, 'm')
+        assert _parse_time_delta_string('2s') == np.timedelta64(2, 's')
+        assert _parse_time_delta_string('20ms') == np.timedelta64(20, 'ms')
+        assert _parse_time_delta_string('20us') == np.timedelta64(20, 'us')
+        assert _parse_time_delta_string('20ns') == np.timedelta64(20, 'ns')
+        assert _parse_time_delta_string('20ps') == np.timedelta64(20, 'ps')
+        assert _parse_time_delta_string('20fs') == np.timedelta64(20, 'fs')
+        assert _parse_time_delta_string('20as') == np.timedelta64(20, 'as')
+
+        with pytest.raises(ValueError):
+            _parse_time_delta_string('')
+
+        with pytest.raises(ValueError):
+            _parse_time_delta_string('   ')
+
+        with pytest.raises(ValueError):
+            _parse_time_delta_string('m')
+
+        with pytest.raises(ValueError):
+            _parse_time_delta_string('1')
+
+        with pytest.raises(ValueError):
+            _parse_time_delta_string('1Y')
+
+    def test_subtract_time_delta(self):
+        meta = Metadata.create(
+            """
+            date: 2024-01-16
+            title: NH3 oxidation over Pt
+
+            start: ${meta.subtract-timedelta:2024-01-16T12:00,12m}
+            """
+        )
+        assert meta.start == '2024-01-16T11:48'
+
+    def test_subtract_time_delta2(self):
+        meta = Metadata.create(
+            """
+            date: 2024-01-16
+            title: NH3 oxidation over Pt
+
+            start: ${meta.subtract-timedelta:2024-01-16T12:00,12ms}
+            """
+        )
+        assert meta.start == '2024-01-16T11:59:59.988'
+
+    def test_nested_resolvers(self):
+        meta = Metadata.create(
+            """
+            date: 2024-01-16
+            title: NH3 oxidation over Pt
+
+            start: 2024-01-16T12:00
+            data:
+              - nested: ${meta.subtract-timedelta:${meta.get:start},12m}
+            """
+        )
+        assert meta.data[0].nested == '2024-01-16T11:48'
